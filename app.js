0c5d6512462e6a6d38d0dba51d05c0bc7132a7ad
Joshua Prodahl <jprodahl@gmail.com>
2016-10-15 23:00:04 -0500

    mongoose

--- app.js
+++ app.js
@@ -5,12 +5,23 @@ global._ = require('lodash');
 global.Promise = require('bluebird');
 
 var config = require('./server/config/config.js');
+var app = Promise.promisifyAll(require('./server/config/express'));
+var server = Promise.promisifyAll(require('http').Server(app));
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
 console.log(`server\t starting in ${config.environment} mode...`);
 
-var app = Promise.promisifyAll(require('./server/config/express'));
-var server = Promise.promisifyAll(require('http').Server(app));
-return server.listenAsync(config.express.port)
-	.then(err => {
-		if (err) process.exit(10);
+var databaseUrl = `mongodb://${config.db.host}:${config.db.port}/${config.db.name}`;
+mongoose.connect(databaseUrl)
+	.then(() => {
+		console.log(`db\t\t connected at mongodb://${config.db.host}:${config.db.port}/${config.db.name}`);
+		return server.listenAsync(config.express.port);
+	})
+	.then(() => {
+		console.log(`server\t hosted at http://${config.express.ip}:${config.express.port}`);
+	})
+	.catch(err => {
+		console.error(err);
+		process.exit(10);
 	});
