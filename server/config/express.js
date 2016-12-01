9950e491aefa8e3eb8fd4e1ce84bf1e89d2425de
Joshua Prodahl <jprodahl@gmail.com>
2016-12-01 13:02:42 -0600

    session

--- server/config/express.js
+++ server/config/express.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
+var config = require('./config');
 
 // put publicly accessible directories here
 app.use('/dist', express.static('dist'));
 app.use('/node_modules', express.static('node_modules'));
+
+// setup session
+app.use(session({
+	secret: '$s3cr3tPassword-replace-this',
+	resave: false,
+	saveUninitialized: true,
+	store: new MongoStore({
+		url: `mongodb://${config.db.host}:${config.db.port}/${config.db.name}`
+	})
+}));
 
 app.set('view engine', 'ejs');
 app.set('views', './server/views');
